8c0042c859c90e6e540d6c3b790dd2be7f8f5091
voozen <voozen3@gmail.com>
2024-04-24 13:48:25 +0200

    init

---
+++ src/test/java/com/sbr/sbr_site/SbrSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbr.sbr_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbrSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
