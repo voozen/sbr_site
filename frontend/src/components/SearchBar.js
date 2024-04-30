d0165a2bb770ab51fa07aa64ee557fcb92ed207a
voozen <voozen3@gmail.com>
2024-04-30 23:04:47 +0200

    frontend endpoint added

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function SearchBarLabel() {
     return (
-        <>
-            <div>
-                <span style={{fontSize: '1.5em', fontWeight: '600'}}>
-                Znajdź lekarza i umów<br></br>
-                wizytę<br></br>
-                </span>
-                <span style={{fontSize: '1.4em', color: '#C0C0C0', fontWeight: '600'}}>
-                Wybierz spośród tysięcy<br></br>
+        <div>
+            <span style={{fontSize: '1.5em', fontWeight: '600'}}>
+                Znajdź lekarza i umów<br />
+                wizytę<br />
+            </span>
+            <span style={{fontSize: '1.4em', color: '#C0C0C0', fontWeight: '600'}}>
+                Wybierz spośród tysięcy<br />
                 specjalistów z całej Polski
-                </span>
-            </div>
-        </>
+            </span>
+        </div>
     );
 }
 
@@ -60,9 +58,15 @@ function CityInputField({ filterText, onFilterTextChange }) {
     );
 }
 
-function SearchButton() {
+function SearchButton({ setDoctors }) {
     function handleClick() {
-
+        axios.get('http://localhost:8080/searchDoctor')
+            .then(response => {
+                setDoctors(response.data);
+            })
+            .catch(error => {
+                console.error('There was a problem with your axios operation:', error);
+            });
     }
 
     return (
@@ -75,42 +79,65 @@ function SearchButton() {
                     border: '0.1em solid #666',
                     background: '#FFF'
                 }}
-                onClick={() => handleClick()}>
+                onClick={handleClick}>
                 Szukaj
             </button>
         </div>
     );
 }
 
-function PrototypeResultList() {
+function DoctorTable({ doctors }) {
     return (
-        <>
-        </>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Imię</th>
+                    <th>Nazwisko</th>
+                    <th>Adres ID</th>
+                    <th>Numer telefonu</th>
+                    <th>Miasto</th>
+                    <th>Kategoria</th>
+                </tr>
+            </thead>
+            <tbody>
+                {doctors.map(doctor => (
+                    <tr key={doctor.doctorID}>
+                        <td>{doctor.doctorID}</td>
+                        <td>{doctor.firstName}</td>
+                        <td>{doctor.lastName}</td>
+                        <td>{doctor.addressID}</td>
+                        <td>{doctor.phoneNumber}</td>
+                        <td>{doctor.city}</td>
+                        <td>{doctor.category}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     );
 }
 
 function SearchBar() {
     const [mainFilterText, setMainFilterText] = useState('');
     const [cityFilterText, setCityFilterText] = useState('');
+    const [doctors, setDoctors] = useState([]);
 
     return (
-        <>
-            <div style={{display: 'grid', justifyContent: 'center'}}>
-                <SearchBarLabel />
-                <div>
-                    <MainInputField 
-                        filterText={mainFilterText}
-                        onFilterTextChange={setMainFilterText}
-                    />
-                    <CityInputField
-                        filterText={cityFilterText}
-                        onFilterTextChange={setCityFilterText}
-                    />
-                </div>
-                <SearchButton />
+        <div style={{display: 'grid', justifyContent: 'center'}}>
+            <SearchBarLabel />
+            <div>
+                <MainInputField 
+                    filterText={mainFilterText}
+                    onFilterTextChange={setMainFilterText}
+                />
+                <CityInputField
+                    filterText={cityFilterText}
+                    onFilterTextChange={setCityFilterText}
+                />
             </div>
-            <PrototypeResultList />
-        </>
+            <SearchButton setDoctors={setDoctors} />
+            <DoctorTable doctors={doctors} />
+        </div>
     );
 }
 
---
+++ src/main/java/tech/grypsiarze/CorsConfig.java
@@ -0,0 +1,17 @@
+package tech.grypsiarze;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Dodaj adres URL Twojej aplikacji React
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Dodaj dozwolone metody HTTP
+                .allowedHeaders("*"); // Dodaj dozwolone nagłówki (możesz również wymienić je ręcznie)
+    }
+}
