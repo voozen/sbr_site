d071d57edb207e55c5f5f32e3cbdbac3aae042a1
voozen <voozen3@gmail.com>
2024-04-30 12:48:20 +0200

    added prototype routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
-    <div className="App">
-      APP
+    <div className='container'>
+      <div className='navigation'>
+        <nav className='navbar'>
+          <ul className='navbar-option'>
+            <Link className='navbar-link' to="/">
+              home
+            </Link>
+          </ul>
+          <ul className='navbar-option'>
+            <Link className='navbar-link' to="/about">
+              about
+            </Link>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+    return (
+        <>
+            <div className='container'>
+                About
+            </div>
+        </>
+    );
+}
+
+export default About;
