df58e87d3b9af34f78bcc911c7f1dcb38ddf141f
voozen <voozen3@gmail.com>
2024-05-01 20:52:03 +0200

    added register page, changed react-router

---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Register() {
+    return (
+        <>
+            <div style={{display: 'grid', width: '100%'}}>
+                sign up
+            </div>
+        </>
+    );
+}
+
+export default Register;
